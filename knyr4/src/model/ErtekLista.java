74d3c8bddd52a9d93e02f89813524e950717d880
focista86 <singermarcell86@gmail.com>
2016-04-08 16:32:41 +0200

    modellek
    érték lista és szerzodés

---
+++ knyr4/src/model/ErtekLista.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.math.BigDecimal;
+
+/**
+ *
+ * @author Marcell
+ */
+public class ErtekLista {
+    
+    private BigDecimal id;
+    private String nev;
+
+    public ErtekLista(BigDecimal id, String nev) {
+        this.id = id;
+        this.nev = nev;
+    }
+
+    public BigDecimal getId() {
+        return id;
+    }
+
+    public void setId(BigDecimal id) {
+        this.id = id;
+    }
+
+    public String getNev() {
+        return nev;
+    }
+
+    public void setNev(String nev) {
+        this.nev = nev;
+    }
+    
+    
+    
+}
