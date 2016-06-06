df581946d21273400297738a763795738567a9d7
marczell <marczellzsofia@yahoo.com>
2016-06-06 12:04:43 +0200

    zsofi

--- KnyrRmi2/src/knyrrmi2/SzerzfelrogzitesController.java
+++ KnyrRmi2/src/knyrrmi2/SzerzfelrogzitesController.java
@@ -21,7 +21,6 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.input.InputMethodEvent;
 import javafx.scene.layout.Background;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
